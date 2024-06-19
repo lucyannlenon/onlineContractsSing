7414fcd4e2901f96d047acb650b0cac424ca6db7
lucyann <lucyann.lenon@gmail.com>
2024-06-19 16:05:05 -0300

    tryte json

--- src/Controller/ContractApiController.php
+++ src/Controller/ContractApiController.php
@@ -6,6 +6,7 @@ use App\DTO\Contract\V1\CreateContractDto;
 use App\Entity\Contracts;
 use App\Services\CreateContractService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
@@ -14,7 +15,7 @@ use Symfony\Component\Routing\Attribute\Route;
 class ContractApiController extends AbstractController
 {
     #[Route('/api/contract/add', name: 'app_contract_create_api', methods: ['POST'])]
-    public function index(#[MapRequestPayload] CreateContractDto $contractDto, CreateContractService $contractService, Request $request ): Response
+    public function index(#[MapRequestPayload] CreateContractDto $contractDto, CreateContractService $contractService, Request $request ): JsonResponse
     {
         $token = $request->query->get('token', null);
         if($token != $_ENV['API_TOKEN'] || $token==null){
@@ -25,7 +26,7 @@ class ContractApiController extends AbstractController
     }
 
     #[Route('/api/contract/{contract}', name: 'app_contract_load_api')]
-    public function load(Contracts $contract, Request $request ): Response
+    public function load(Contracts $contract, Request $request ): JsonResponse
     {
         $token = $request->query->get('token', null);
         if($token != $_ENV['API_TOKEN'] || $token==null){
