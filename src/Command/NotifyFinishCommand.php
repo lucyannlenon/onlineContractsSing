740091dce2c872fa100c144b1d74537146a76790
lucyann <lucyann.lenon@gmail.com>
2024-07-12 11:27:55 -0300

    text bug fix

--- src/Command/NotifyFinishCommand.php
+++ src/Command/NotifyFinishCommand.php
@@ -2,14 +2,10 @@
 
 namespace App\Command;
 
-use App\Scheduler\Task\NotificationFinishContractTask;
-use App\Services\GeneratePdfContract;
 use App\Services\NotificationServer;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
