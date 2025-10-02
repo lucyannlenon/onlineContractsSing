1a100d8ed3260faca277c795d6b9cdd2f6429a73
lucyannl <lucyann.lenon@gmail.com>
2025-10-02 18:06:21 -0300

    create contract with template

---
+++ src/Services/NotificationContractServer.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Services;
+
+use App\Entity\Contracts;
+use App\Enum\NotificationServerEnum;
+use App\Repository\ContractsRepository;
+use Psr\Log\LoggerInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class NotificationContractServer
+{
+    public function __construct(
+        private readonly ContractsRepository $contractsRepository,
+        private readonly HttpClientInterface $httpClient,
+        private readonly LoggerInterface     $logger
+    )
+    {
+
+    }
+
+
+
+
+    /**
+     * @param Contracts $contracts
+     * @return void
+     * @throws \Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface
+     * @throws \Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface
+     * @throws \Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface
+     * @throws \Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface
+     */
+    public function notify(Contracts $contracts): void
+    {
+        $items = $contracts->getSignatures()->toArray();
+        $links = [];
+        foreach ($items as $item) {
+            if (empty($item->getLink())) {
+                return;
+            }
+            $links[] = $item->getLink();
+        }
+        $post = [
+            'cpf' => $contracts->getCpf(),
+            'accessKey' => $contracts->getAccessKey(),
+            'links' => $links,
+            'action' => NotificationServerEnum::FINISH->name,
+            'contractType' => $contracts->getContractType()
+        ];
+
+        try {
+            $this->sendToServer($post);
+            $contracts->setNotified(true);
+            $this->contractsRepository->save($contracts);
+        } catch (\Exception $exception) {
+            $this->logger->critical($exception->getMessage(), [
+                'cause' => $exception
+            ]);
+        }
+    }
+
+    /**
+     * @param array $links
+     * @param Contracts $contracts
+     * @return void
+     * @throws \Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface
+     * @throws \Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface
+     * @throws \Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface
+     * @throws \Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface
+     */
+    public function sendToServer(array $post): void
+    {
+
+
+        $url = $_ENV['APPSUPORTE_WEBHOOK'];
+
+        $response = $this->httpClient->request('POST', $url, [
+            'query' => [
+                'token' => $_ENV['API_TOKEN']
+            ],
+            'json' => $post
+        ]);
+
+        $response->getContent();
+
+    }
+
+}
--- src/Services/NotificationServer.php
+++ src/Services/NotificationServer.php
@@ -8,12 +8,11 @@ use App\Repository\ContractsRepository;
 use Psr\Log\LoggerInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
-class NotificationServer
+readonly class NotificationServer
 {
     public function __construct(
-        private readonly ContractsRepository $contractsRepository,
-        private readonly HttpClientInterface $httpClient,
-        private readonly LoggerInterface     $logger
+        private ContractsRepository        $contractsRepository,
+        private NotificationContractServer $notificationContractServer
     )
     {
 
@@ -34,71 +33,8 @@ class NotificationServer
         ]);
 
         foreach ($items as $item) {
-            $this->notify($item);
+            $this->notificationContractServer->notify($item);
         }
-
-    }
-
-    /**
-     * @param Contracts $contracts
-     * @return void
-     * @throws \Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface
-     * @throws \Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface
-     * @throws \Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface
-     * @throws \Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface
-     */
-    private function notify(Contracts $contracts): void
-    {
-        $items = $contracts->getSignatures()->toArray();
-        $links = [];
-        foreach ($items as $item) {
-            if (empty($item->getLink())) {
-                return;
-            }
-            $links[] = $item->getLink();
-        }
-        $post = [
-            'cpf' => $contracts->getCpf(),
-            'accessKey' => $contracts->getAccessKey(),
-            'links' => $links,
-            'action' => NotificationServerEnum::FINISH->name,
-            'contractType' => $contracts->getContractType()
-        ];
-
-        try {
-            $this->sendToServer($post);
-            $contracts->setNotified(true);
-            $this->contractsRepository->save($contracts);
-        } catch (\Exception $exception) {
-            $this->logger->critical($exception->getMessage(), [
-                'cause' => $exception
-            ]);
-        }
-    }
-
-    /**
-     * @param array $links
-     * @param Contracts $contracts
-     * @return void
-     * @throws \Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface
-     * @throws \Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface
-     * @throws \Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface
-     * @throws \Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface
-     */
-    public function sendToServer(array $post): void
-    {
-
-
-        $url = $_ENV['APPSUPORTE_WEBHOOK'];
-
-        $response = $this->httpClient->request('POST', $url, [
-            'query' => [
-                'token' => $_ENV['API_TOKEN']
-            ],
-            'json' => $post
-        ]);
-
-        $response->getContent();
 
     }
 
