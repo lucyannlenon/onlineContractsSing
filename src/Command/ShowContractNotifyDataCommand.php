b94bd0a5880a5dfc56869a388f2ddc01e526edbd
lucyannl <lucyann.lenon@gmail.com>
2025-10-02 11:38:34 -0300

    create contract with template

---
+++ src/Command/ShowContractNotifyDataCommand.php
@@ -0,0 +1,62 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Command;
+
+use App\Enum\NotificationServerEnum;
+use App\Repository\ContractsRepository;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Output\OutputInterface;
+
+#[AsCommand(name: 'app:show_contract:notifyData', description: 'show contracts by accept key')]
+class ShowContractNotifyDataCommand extends Command
+{
+    public function __construct(
+        private readonly ContractsRepository $contractsRepository,
+
+        ?string                              $name = null)
+    {
+        parent::__construct($name);
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->addArgument('accessKey', InputArgument::REQUIRED, 'Access key for the contract')
+            ->addArgument('cpf', InputArgument::REQUIRED, 'CPF number');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $items = $this->contractsRepository->findBy([
+            'accessKey' => $input->getArgument('accessKey'),
+            'cpf' => $input->getArgument('cpf')
+        ]);
+
+
+        foreach ($items as $contracts) {
+            $itemsLinke = $contracts->getSignatures()->toArray();
+            $links = [];
+            foreach ($itemsLinke as $item) {
+                if (empty($item->getLink())) {
+                    continue;
+                }
+                $links[] = $item->getLink();
+            }
+            $post = [
+                'cpf' => $contracts->getCpf(),
+                'accessKey' => $contracts->getAccessKey(),
+                'links' => $links,
+                'action' => NotificationServerEnum::FINISH->name,
+                'contractType' => $contracts->getContractType()
+            ];
+            dump($post);
+        }
+
+        return Command::SUCCESS;
+    }
+}
